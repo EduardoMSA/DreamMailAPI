cfeb9483a2c6ea632fc2bd9932805ea5567a1726
Eduardo Mendez <eduardomndez@ymail.com>
2021-11-29 12:12:09 -0600

    CORS ADDED

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 from endpodints.mail import MailSender, MailReciever, MailReader
 from endpodints.news import News, NewsSources
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app, origins="http://127.0.0.1:8080", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+    supports_credentials=True)
 
 api.add_resource(MailSender, '/mail/send')
 api.add_resource(MailReciever, '/mail/fetch')
